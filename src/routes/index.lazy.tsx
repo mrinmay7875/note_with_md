f49ac80e7b062dba279ca1b19d8259d3a908c170
Mrinmay Santra <mrinmay7875@gmail.com>
2024-07-28 00:47:56 +0530

    Added SearchBox UI for Home page

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,7 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
 import NoteCard from './../components/NoteCard';
+import { Field, SearchBox } from '@fluentui/react-components';
+import './Home.css';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -7,7 +9,10 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <div className='p-2'>
+    <div className='mainContainer'>
+      <Field className='m-50' label='Search for Notes..'>
+        <SearchBox />
+      </Field>
       <h3>This is the Index page where we will all the notes</h3>
       <NoteCard title='Note 1' tags={['tag1', 'tag2']} />
     </div>
