0399aa0fbe2f238bce8ee0592f365b890c811e89
Mrinmay Santra <mrinmay7875@gmail.com>
2024-07-28 10:38:12 +0530

    removed Counter slice

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -3,29 +3,19 @@ import NoteCard from '../components/NoteCard/NoteCard';
 import { Field, SearchBox } from '@fluentui/react-components';
 import './Home.css';
 import AddNewNoteDialog from '../components/NewNoteDialog/AddNewNoteDialog';
-import { useDispatch, useSelector } from 'react-redux';
-import { decrement, increment } from '../slice/counterSlice';
-import store from '../store/store';
+import { useSelector } from 'react-redux';
+import { Note } from '../types/type';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
 });
 
 function Index() {
-  const count = useSelector((state: any) => state.counter.value);
   const notes = useSelector((state: any) => state.notes);
-  console.log('notes', notes);
+  // console.log('notes inside me', notes);
 
-  store.subscribe(() => {
-    console.log(store.getState());
-  });
-
-  const dispatch = useDispatch();
   return (
     <div className='mainContainer'>
-      <h1>Count: {count}</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
       <Field className='m-50'>
         {/* TODO:  Increase the width of the search box */}
         <SearchBox placeholder='Search for notes..' />
@@ -34,8 +24,12 @@ function Index() {
         <br />
         <AddNewNoteDialog />
       </Field>
-      <h3>This is the Index page where we will all the notes</h3>
-      <NoteCard title='Note 1' tags={['tag1', 'tag2']} />
+      {notes.map((note: Note) => (
+        <div>
+          <NoteCard key={note.id} title={note.title} tags={['react']} />
+          <br />
+        </div>
+      ))}
     </div>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,9 @@
 // app/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../slice/counterSlice';
 import notesReducer from '../slice/noteSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
     notes: notesReducer,
   },
 });
