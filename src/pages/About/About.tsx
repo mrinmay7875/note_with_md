c937c4be62b714ff336ecd61b8bb80df9c4ecba7
Mrinmay Santra <mrinmay7875@gmail.com>
2024-07-28 23:14:00 +0530

    Reorganized folder structure for routes

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,5 @@
+function About() {
+  return <div className='p-2'>This is the About page</div>;
+}
+
+export default About;
--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -1,9 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
+import About from '../pages/About/About';
 
 export const Route = createLazyFileRoute('/about')({
   component: About,
 });
-
-function About() {
-  return <div className='p-2'>This is the About page</div>;
-}
